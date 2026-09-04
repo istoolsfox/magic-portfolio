5f394ecfa3256ec44af0f9f19e8eceba10d36143
istoolsfox <istoolsfox@users.noreply.github.com>
2026-09-04 16:23:59 +0800

    chore: deploy 2026-09-04 16:23

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Row, IconButton } from "@once-ui-system/core";
+import { Row, IconButton, Text } from "@once-ui-system/core";
 import { social } from "@/resources";
 import { WeChatQRCard } from "./WeChatQRCard";
 import styles from "./Footer.module.scss";
@@ -6,82 +6,68 @@ import Image from "next/image";
 
 export const Footer = () => {
   return (
-    <Row as="footer" fillWidth padding="8" horizontal="center" s={{ direction: "column" }}>
-      <Row
-        className={styles.mobile}
-        maxWidth="m"
-        paddingY="8"
-        paddingX="16"
-        gap="16"
-        horizontal="center"
-        vertical="center"
-        s={{
-          direction: "column",
-          horizontal: "center",
-          align: "center",
-        }}
-      >
-        <Row gap="32">
-          {social.map((item) => {
-            if (!item.link) return null;
-            const button = (
-              <IconButton
-                key={item.name}
-                href={item.link}
-                icon={item.icon}
-                tooltip={item.name === "WeChat" ? undefined : item.name}
-                size="s"
-                variant="ghost"
-              />
-            );
-            return item.name === "WeChat" ? (
-              <WeChatQRCard key={item.name}>{button}</WeChatQRCard>
-            ) : (
-              button
-            );
-          })}
-        </Row>
+    <Row
+      as="footer"
+      fillWidth
+      direction="column"
+      horizontal="center"
+      vertical="center"
+      padding="8"
+      gap="4"
+      s={{ direction: "column" }}
+    >
+      <Row paddingY="8" paddingX="16" gap="32" horizontal="center" vertical="center">
+        {social.map((item) => {
+          if (!item.link) return null;
+          const button = (
+            <IconButton
+              key={item.name}
+              href={item.link}
+              icon={item.icon}
+              tooltip={item.name === "WeChat" ? undefined : item.name}
+              size="s"
+              variant="ghost"
+            />
+          );
+          return item.name === "WeChat" ? (
+            <WeChatQRCard key={item.name}>{button}</WeChatQRCard>
+          ) : (
+            button
+          );
+        })}
       </Row>
       <Row
-        fillWidth
-        paddingY="16"
+        paddingY="8"
         paddingX="16"
+        gap="8"
+        wrap
         horizontal="center"
         vertical="center"
-        s={{ direction: "column", align: "center" }}
+        s={{ direction: "row", horizontal: "center", align: "center" }}
       >
-        <Row gap="4" vertical="center" horizontal="center">
-          <Image
-            src="/images/beian.png"
-            alt="备案图标"
-            width={15}
-            height={16}
-          />
-          <a
-            href="https://beian.miit.gov.cn/"
-            target="_blank"
-            rel="noreferrer"
-            style={{ color: "inherit", textDecoration: "none" }}
-          >
+        <a
+          href="https://beian.miit.gov.cn/"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.beianLink}
+        >
+          <Image src="/images/beian.png" alt="" width={14} height={15} />
+          <Text as="span" variant="label-default-s" onBackground="neutral-weak">
             浙ICP备2026047392号-1
-          </a>
-        </Row>
-        <Row gap="4" vertical="center" horizontal="center">
-          <Image
-            src="/images/beian.png"
-            alt="公安备案图标"
-            width={15}
-            height={16}
-          />
-          <a
-            href="https://beian.mps.gov.cn/#/query/webSearch?code=33010902004850"
-            target="_blank"
-            rel="noreferrer"
-            style={{ color: "inherit", textDecoration: "none" }}
-          >
+          </Text>
+        </a>
+        <Row width="1" height="12" background="neutral-alpha-medium" />
+        <a
+          href="https://beian.mps.gov.cn/#/query/webSearch?code=33010902004850"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.beianLink}
+        >
+          <Image src="/images/beian.png" alt="" width={14} height={15} />
+          <Text as="span" variant="label-default-s" onBackground="neutral-weak">
             浙公网安备33010902004850号
-          </a>
-        </Row>
+          </Text>
+        </a>
       </Row>
       <Row height="80" hide s={{ hide: false }} />
     </Row>
